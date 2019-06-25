6942f6c07aa54b57ba20ee8b298ed0d2fdbce2a5
Adrian-Jablonski <Jablonski.Adrian1@gmail.com>
2019-06-24 19:46:40 -0500

    Set up header component

--- React-Basics/app.js
+++ React-Basics/app.js
@@ -1,56 +1,11 @@
-const titleWithReactCreateElement = React.createElement(
-    'h1',
-    { id: 'main-title', title: 'This is a title.' },
-    'My First React Element!'
-);  // Object with type, props, and children
-
-console.log(titleWithReactCreateElement);
-
-const descWithReactCreateElement = React.createElement(
-    'p',
-    null,
-    'I just learned how to create a React node and render it into the DOM.'
-);
-
-const headerWithReactCreateElement = React.createElement(
-    'header',
-    null,
-    title,
-    desc
-);
-
-// Using JSX
-const title = <h1>My First React Element!</h1>;
-console.log(title);
-
-const desc = <p>I just learned how to create a React node and render it into the DOM.</p>
-
-// const header = (
-//     <header>
-//         <h1>My First React Element!</h1>
-//         <p>I just learned how to create a React node and render it into the DOM.</p>
-//     </header>
-// );
-
-const title2 = 'My First React Element!';
-const desc2 = 'I just learned how to create a React node and render it into the DOM';
-const myTitleID = 'main-title';
-const name = 'Adrian';
-
-// Curly braces used to write javascript in JSX
-const header = (
-    <header>
-    {/* This is a comment */}
-        <h1 id={myTitleID}>{ name }'s First React Element!</h1>
-        <p className="main-desc">{ desc2 }</p>
-    </header>
-);
-
-// // Renders title react html to root element
-// ReactDOM.render(
-//     title,
-//     document.getElementById('root')
-// );
+function Header() { // React components are required to begin with a capital letter
+    return (
+        <header>
+            <h1>Scoreboard</h1>
+            <span className="stats">Players: 1</span>
+        </header>
+    );
+}
 
 ReactDOM.render(
     header,
---
+++ React-Basics/notes.js
@@ -0,0 +1,58 @@
+const titleWithReactCreateElement = React.createElement(
+    'h1',
+    { id: 'main-title', title: 'This is a title.' },
+    'My First React Element!'
+);  // Object with type, props, and children
+
+console.log(titleWithReactCreateElement);
+
+const descWithReactCreateElement = React.createElement(
+    'p',
+    null,
+    'I just learned how to create a React node and render it into the DOM.'
+);
+
+const headerWithReactCreateElement = React.createElement(
+    'header',
+    null,
+    title,
+    desc
+);
+
+// Using JSX
+const title = <h1>My First React Element!</h1>;
+console.log(title);
+
+const desc = <p>I just learned how to create a React node and render it into the DOM.</p>
+
+// const header = (
+//     <header>
+//         <h1>My First React Element!</h1>
+//         <p>I just learned how to create a React node and render it into the DOM.</p>
+//     </header>
+// );
+
+const title2 = 'My First React Element!';
+const desc2 = 'I just learned how to create a React node and render it into the DOM';
+const myTitleID = 'main-title';
+const name = 'Adrian';
+
+// Curly braces used to write javascript in JSX
+const header = (
+    <header>
+    {/* This is a comment */}
+        <h1 id={myTitleID}>{ name }'s First React Element!</h1>
+        <p className="main-desc">{ desc2 }</p>
+    </header>
+);
+
+// // Renders title react html to root element
+// ReactDOM.render(
+//     title,
+//     document.getElementById('root')
+// );
+
+ReactDOM.render(
+    header,
+    document.getElementById('root')
+);
