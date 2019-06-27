fa9d6db9eac1c9af943af0a513db9a3ac32c484a
Adrian-Jablonski <Jablonski.Adrian1@gmail.com>
2019-06-26 20:55:07 -0500

    Added PureComponent to players to only rerender the player whose score is changed

--- React-Components/scoreboard/src/components/Player.js
+++ React-Components/scoreboard/src/components/Player.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, {PureComponent } from 'react';
 import Counter from './Counter';
 
-const Player = (props) => {
-    return (
-        <div className="player">
-            <span className="player-name">
-                <button className="remove-player" onClick={() => props.removePlayer(props.id)}>✖</button>
-                {props.name}
-            </span>
-
-            <Counter score={props.score}
-                changeScore={props.changeScore} // Passing handleScoreChage method to Counter component
-                index={props.index}
-            />
-        </div>
-    );
+class Player extends PureComponent {    // PureComponent will only rerender the player whose score is changed. Otherwise, all players would be rerendered when one players score is changed
+    render() {
+        console.log(this.props.name + ' rendered');
+        return (
+            <div className="player">
+                <span className="player-name">
+                    <button className="remove-player" onClick={() => this.props.removePlayer(this.props.id)}>✖</button>
+                    {this.props.name}
+                </span>
+    
+                <Counter score={this.props.score}
+                    changeScore={this.props.changeScore} // Passing handleScoreChage method to Counter component
+                    index={this.props.index}
+                />
+            </div>
+        );
+    }
 }
 
 export default Player;
