94a553e56798b6aa5f5eb2484dc5a814cd77a846
Adrian Jablonski <adrian.jablonski19@gmail.com>
2019-10-24 12:34:52 -0400

    Setup RSVP app with GuestList and Guest components

---
+++ react-by-example/src/App.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import './App.css';
+import GuestList from './components/GuestList';
+
+class App extends Component {
+
+  state = {
+    guests: [
+      {
+        name: 'Treasure',
+        isConfirmed: false
+      },
+      {
+        name: 'Nic',
+        isConfirmed: true
+      },
+      {
+        name: 'Matt',
+        isConfirmed: true
+      }
+    ]
+  }
+
+  toggleConfirmationAt = (indexToChange) => {
+    this.setState({
+      guests: this.state.guests.map((guest, index) => {
+        if (index === indexToChange) {
+          return {
+            ...guest,   // using spread operator to transfer unchanged key values to new object
+            isConfirmed: !guest.isConfirmed
+          }
+        }
+        return guest;
+      })
+    })
+  }
+
+  getTotalInvited = () => {
+    return this.state.guests.length;
+  }
+  // getAttendingGuests
+  // getUnconfirmedGuests
+
+  render() {
+
+    return (
+      <div className="App">
+        <header>
+          <h1>RSVP</h1>
+          <p>A Treehouse App</p>
+          <form>
+            <input type="text" value="Safia" placeholder="Invite Someone" />
+            <button type="submit" name="submit" value="submit">Submit</button>
+          </form>
+        </header>
+        <div class="main">
+          <div>
+            <h2>Invitees</h2>
+            <label>
+              <input type="checkbox"/> Hide those who haven't responded
+            </label>
+          </div>
+          <table className="counter">
+            <tbody>
+              <tr>
+                <td>Attending:</td>
+                <td>2</td>
+              </tr>
+              <tr>
+                <td>Unconfirmed:</td>
+                <td>1</td>
+              </tr>
+              <tr>
+                <td>Total:</td>
+                <td>3</td>
+              </tr>
+            </tbody>
+          </table>
+          <GuestList
+            guests={this.state.guests}
+          ></GuestList>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ react-by-example/src/components/GuestList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Guest from './Guest';
+
+const GuestList = props => {
+    return (
+        <ul>
+            {props.guests.map((guest, index) => {
+                return (
+                    <Guest 
+                        key={index}
+                        name={guest.name}
+                        isConfirmed={guest.isConfirmed}
+                    ></Guest>
+                )
+            })}
+        </ul>
+    )
+}
+
+GuestList.propTypes = {
+    guests: PropTypes.array.isRequired
+}
+
+export default GuestList;
