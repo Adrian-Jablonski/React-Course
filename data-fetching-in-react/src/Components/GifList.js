5cd64acef906bb38162f9a72b215b8fa1236f2fd
Adrian Jablonski <adrian.jablonski19@gmail.com>
2019-10-25 12:33:51 -0400

    Displayed results from API

--- data-fetching-in-react/src/Components/GifList.js
+++ data-fetching-in-react/src/Components/GifList.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Gif from './Gif';
 
-const GifList = props => { 
+const GifList = ({data}) => { 
   
+  let gifs = data.map(gif => 
+      <Gif
+        key={gif.id}
+        url={gif.images.fixed_height.url}
+      ></Gif>
+    )
+
   return(
     <ul className="gif-list">
-      {/* <Gif /> */}
+      {gifs}
     </ul> 
   );
 }
