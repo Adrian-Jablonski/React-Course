aa71c233318662fe4b22f89fddd5e497c50d4489
Adrian Jablonski <adrian.jablonski19@gmail.com>
2019-11-05 16:47:08 -0500

    Setup 404 page

--- Complete-React-Dev-Course/expensify/src/app.js
+++ Complete-React-Dev-Course/expensify/src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -28,15 +28,23 @@ const HelpPage = () => (
     </div>
 )
 
+const NotFoundPage = () => (
+    <div>
+        404!
+    </div>
+)
+
 
 const routes = (
     <BrowserRouter>
-        <div>
-            <Route path="/" component={ExpenseDashboardPage} exact={true}/>
-            <Route path="/create" component={AddExpensePage} exact={true}/>
-            <Route path="/edit" component={EditExpensePage} exact={true}/>
-            <Route path="/help" component={HelpPage} exact={true}/>
-        </div>
+        <Switch>
+            <Route path="/" component={ExpenseDashboardPage} exact={true} />
+            <Route path="/create" component={AddExpensePage} exact={true} />
+            <Route path="/edit" component={EditExpensePage} exact={true} />
+            <Route path="/help" component={HelpPage} exact={true} />
+            {/* 404 page */}
+            <Route component={NotFoundPage} />
+        </Switch>
     </BrowserRouter>
 );
 
