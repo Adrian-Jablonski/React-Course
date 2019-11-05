82004e04aa66d05d0bbad279ef1a7d394263d8f5
Adrian Jablonski <adrian.jablonski19@gmail.com>
2019-11-05 18:38:39 -0500

    Organized Routes into component and router files

--- Complete-React-Dev-Course/expensify/src/app.js
+++ Complete-React-Dev-Course/expensify/src/app.js
@@ -1,66 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-const ExpenseDashboardPage = () => (
-    <div>
-        Dashboard component
-    </div>
-)
-
-const AddExpensePage = () => (
-    <div>
-        Add Expense component
-    </div>
-)
-
-const EditExpensePage = () => (
-    <div>
-        Edit Expense component
-    </div>
-)
-
-const HelpPage = () => (
-    <div>
-        Help component
-    </div>
-)
-
-// Link tag redirects without page refresh
-const NotFoundPage = () => (
-    <div>
-        404 - <Link to="/">Go Home</Link>
-    </div>
-)
-
-//NavLink can create a class for the active link with the activeClassName property
-const Header = () => (
-    <header>
-        <h1>Expensify</h1>
-        <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
-        <NavLink to="/create" activeClassName="is-active" exact={true}>Create Expense</NavLink>
-        <NavLink to="/edit" activeClassName="is-active" exact={true}>Edit</NavLink>
-        <NavLink to="/help" activeClassName="is-active" exact={true}>Help</NavLink>
-    </header>
-)
-
-
-const routes = (
-    <BrowserRouter>
-        <div>
-            <Header/>
-            <Switch>
-                <Route path="/" component={ExpenseDashboardPage} exact={true} />
-                <Route path="/create" component={AddExpensePage} exact={true} />
-                <Route path="/edit" component={EditExpensePage} exact={true} />
-                <Route path="/help" component={HelpPage} exact={true} />
-                {/* 404 page */}
-                <Route component={NotFoundPage} />
-            </Switch>
-        </div>
-    </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
---
+++ Complete-React-Dev-Course/expensify/src/routers/AppRouter.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import AddExpensePage from '../components/AddExpensePage';
+import EditExpensePage from '../components/EditExpensePage';
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
+import Header from '../components/Header';
+import HelpPage from '../components/HelpPage';
+import NotFoundPage from '../components/NotFoundPage';
+
+const AppRouter = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <Header />
+                <Switch>
+                    <Route path="/" component={ExpenseDashboardPage} exact={true} />
+                    <Route path="/create" component={AddExpensePage} exact={true} />
+                    <Route path="/edit" component={EditExpensePage} exact={true} />
+                    <Route path="/help" component={HelpPage} exact={true} />
+                    {/* 404 page */}
+                    <Route component={NotFoundPage} />
+                </Switch>
+            </div>
+        </BrowserRouter>
+    )
+}
+
+export default AppRouter;
