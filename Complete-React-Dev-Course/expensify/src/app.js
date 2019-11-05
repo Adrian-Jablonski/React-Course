8060199069f9e6913ee7870b7a42679472cbb07c
Adrian Jablonski <adrian.jablonski19@gmail.com>
2019-11-05 18:26:37 -0500

    Added client side routing with Link and NavLink which redirect without a page refresh

--- Complete-React-Dev-Course/expensify/src/app.js
+++ Complete-React-Dev-Course/expensify/src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -28,23 +28,38 @@ const HelpPage = () => (
     </div>
 )
 
+// Link tag redirects without page refresh
 const NotFoundPage = () => (
     <div>
-        404!
+        404 - <Link to="/">Go Home</Link>
     </div>
+)
+
+//NavLink can create a class for the active link with the activeClassName property
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
+        <NavLink to="/create" activeClassName="is-active" exact={true}>Create Expense</NavLink>
+        <NavLink to="/edit" activeClassName="is-active" exact={true}>Edit</NavLink>
+        <NavLink to="/help" activeClassName="is-active" exact={true}>Help</NavLink>
+    </header>
 )
 
 
 const routes = (
     <BrowserRouter>
-        <Switch>
-            <Route path="/" component={ExpenseDashboardPage} exact={true} />
-            <Route path="/create" component={AddExpensePage} exact={true} />
-            <Route path="/edit" component={EditExpensePage} exact={true} />
-            <Route path="/help" component={HelpPage} exact={true} />
-            {/* 404 page */}
-            <Route component={NotFoundPage} />
-        </Switch>
+        <div>
+            <Header/>
+            <Switch>
+                <Route path="/" component={ExpenseDashboardPage} exact={true} />
+                <Route path="/create" component={AddExpensePage} exact={true} />
+                <Route path="/edit" component={EditExpensePage} exact={true} />
+                <Route path="/help" component={HelpPage} exact={true} />
+                {/* 404 page */}
+                <Route component={NotFoundPage} />
+            </Switch>
+        </div>
     </BrowserRouter>
 );
 
