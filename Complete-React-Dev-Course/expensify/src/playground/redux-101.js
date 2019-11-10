502f15a208f759522e0c807512a7649dd6445f5b
Adrian Jablonski <adrian.jablonski19@gmail.com>
2019-11-10 09:17:01 -0500

    Refactored actions into action generators

--- Complete-React-Dev-Course/expensify/src/playground/redux-101.js
+++ Complete-React-Dev-Course/expensify/src/playground/redux-101.js
@@ -1,16 +1,44 @@
 import { createStore } from 'redux';
 
+// Action generators - functions that return action objects
+const incrementCount = ({incrementBy = 1} = {}) => {
+    return {
+        type: 'INCREMENT',
+        // incrementBy: (incrementBy) ? incrementBy : 1
+        incrementBy
+    }
+}
+
+const decremantCount = ({decrementBy = 1} = {}) => {
+    return {
+        type: 'DECREMENT',
+        decrementBy
+    }
+}
+
+const setCount = ({count}) => {
+    return {
+        type: 'SET',
+        count
+    }
+}
+
+const reset = () => {
+    return {
+        type: 'RESET',
+        count: 0
+    }
+}
+
 const store = createStore((state = { count: 0 }, action) => {
     switch (action.type) {
         case 'INCREMENT':
-            const incrementBy = (action.incrementBy) ? action.incrementBy : 1
             return {
-                count: state.count + incrementBy
+                count: state.count + action.incrementBy
             }
         case 'DECREMENT':
-            const decrementBy = (action.decrementBy) ? action.decrementBy : 1
             return {
-                count: state.count - decrementBy
+                count: state.count - action.decrementBy
             }
         case 'SET':
             return {
@@ -18,7 +46,7 @@ const store = createStore((state = { count: 0 }, action) => {
             }
         case 'RESET':
             return {
-                count: 0
+                count: action.count
             }
         default:
             return state;
@@ -32,31 +60,24 @@ store.subscribe(() => {
 
 // Actions - an object that gets sent to the store
 
-// Increment the count
-store.dispatch({
-    type: 'INCREMENT',
-    incrementBy: 5  // used to pass dynamic data
-});
+// // Increment the count
+// store.dispatch({
+//     type: 'INCREMENT',
+//     incrementBy: 5  // used to pass dynamic data
+// });
 
-store.dispatch({
-    type: 'INCREMENT'
-});
+// store.dispatch({
+//     type: 'INCREMENT'
+// });
+
+store.dispatch(incrementCount({incrementBy: 5}));
+store.dispatch(incrementCount());
 
 // RESET - set the count equal to zero
-store.dispatch({
-    type: 'RESET'
-});
+store.dispatch(reset());
 
-store.dispatch({
-    type: 'DECREMENT'
-});
+store.dispatch(decremantCount());
 
-store.dispatch({
-    type: 'DECREMENT',
-    decrementBy: 10
-});
+store.dispatch(decremantCount({type: 'DECREMENT', decrementBy: 10}));
 
-store.dispatch({
-    type: 'SET',
-    count: 101
-})
+store.dispatch(setCount({count: 105}));
