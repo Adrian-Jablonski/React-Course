8bb23a885fa6dbda2ead3b533cdec3bc80daac91
Adrian Jablonski <adrian.jablonski19@gmail.com>
2019-12-05 18:16:01 -0500

    Created a Higher Order Component example

---
+++ Complete-React-Dev-Course/expensify/src/playground/higherOrderComponent.js
@@ -0,0 +1,49 @@
+// Higher Order Component (HOC) - A component that renders another componen
+// Reuse code
+// Render hijacking
+// Prop manipulation
+// Abstract state
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Info = ({ info }) => {
+    return (
+        <div>
+            <h1>Info</h1>
+            <p>The info is : {info}</p>
+        </div>
+    )
+}
+
+const withAdminWarning = (WrappedComponent) => {
+    return (props) => {
+        return (
+            <div>
+                {props.isAdmin && <p>This is private info. Please don't share!</p>}
+                
+                <WrappedComponent {...props}></WrappedComponent>
+            </div>
+        )
+    }
+}
+
+const requireAuthentication = (WrappedComponent) => {
+    return (props) => {
+        return (
+            <div>
+                {(props.isAuthenticated) 
+                ? 
+                    (<WrappedComponent {...props}></WrappedComponent>)
+                : 
+                    (<p>Please Login</p>)}
+            </div>
+        )
+    }
+}
+
+const AdminInfo = withAdminWarning(Info);
+const AuthInfo = requireAuthentication(Info);
+
+//ReactDOM.render(<AdminInfo isAdmin={true} info="Details"></AdminInfo>, document.getElementById('app'));
+ReactDOM.render(<AuthInfo isAuthenticated={true} info="Details"></AuthInfo>, document.getElementById('app'));
