e87e974b4920292ebb3ab9f68a92b21a2f883184
Adrian Jablonski <adrian.jablonski19@gmail.com>
2019-12-05 18:31:19 -0500

    Setup Provider & Setup connected higher order components

--- Complete-React-Dev-Course/expensify/src/app.js
+++ Complete-React-Dev-Course/expensify/src/app.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './redux/store/configureStore';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-import {addExpense} from './redux/actions/expenses';
-import {setTextFilter} from './redux/actions/filters';
+import { addExpense } from './redux/actions/expenses';
+import { setTextFilter } from './redux/actions/filters';
 
 import getVisibleExpenses from './redux/selectors/expenses';
 
 const store = configureStore();
 
-store.dispatch(addExpense({description: 'Water bill'}))
-store.dispatch(addExpense({description: 'Gas bill'}))
-
-console.log(store.getState());
-
-store.dispatch(setTextFilter('bill'));
-
-console.log(getVisibleExpenses(store.getState().expenses, store.getState().filters));
+store.dispatch(addExpense({ description: 'Water bill' }))
+store.dispatch(addExpense({ description: 'Gas bill' }))
 
 store.dispatch(setTextFilter('water'));
 
 console.log(getVisibleExpenses(store.getState().expenses, store.getState().filters));
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+// Provider gives access to the Redux store to all components that need to use it and that use Connect to access the store
+const jsx = (
+    <Provider store={store}>
+        <AppRouter></AppRouter>
+    </Provider>
+)
+
+
+ReactDOM.render(jsx, document.getElementById('app'));
--- Complete-React-Dev-Course/expensify/webpack.config.js
+++ Complete-React-Dev-Course/expensify/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path'); // used to join paths for multiple OS compatability
 
 module.exports = {
-    entry: './src/playground/higherOrderComponent',
+    entry: './src/app.js',
     output: {
         path: path.join(__dirname, 'public'),   // __dirname gives the absolute path on the current users computer
         filename: 'bundle.js'
