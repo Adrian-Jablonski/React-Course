817b628bae6de72bafc6c6de9a79df961e7d1a38
Adrian Jablonski <adrian.jablonski19@gmail.com>
2019-12-05 19:07:32 -0500

    Rendered expenses to screen by using expense selector in mapStateToProps

--- Complete-React-Dev-Course/expensify/src/app.js
+++ Complete-React-Dev-Course/expensify/src/app.js
@@ -13,7 +13,7 @@ import getVisibleExpenses from './redux/selectors/expenses';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill' }))
+store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }))
 store.dispatch(addExpense({ description: 'Gas bill' }))
 
 store.dispatch(setTextFilter('water'));
--- Complete-React-Dev-Course/expensify/src/components/ExpenseList.js
+++ Complete-React-Dev-Course/expensify/src/components/ExpenseList.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import ExpenseListItem from './ExpenseListItem';
+import selectExpenses from '../redux/selectors/expenses';
+
 const ExpenseList = (props) => {
     return (
         <div>
             <h1>Expense List</h1>
-            {props.filters.text}
-            {props.expenses.length}
+            {props.expenses.map((expense, index) => {
+                return (
+                    <ExpenseListItem
+                        key={index}
+                        {...expense}    // Spreads out all properties on expense and passes description, amount, and createdAt
+                    ></ExpenseListItem>
+                )
+            })}
         </div>
     )
 };
 
 const mapStateToProps = (state) => {
     return {
-        expenses: state.expenses,
-        filters: state.filters
+        // expenses: state.expenses,
+        // filters: state.filters
+        expenses: selectExpenses(state.expenses, state.filters)
     }
 }
 
