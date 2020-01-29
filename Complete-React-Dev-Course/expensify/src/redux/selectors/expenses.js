5ed3f7fd40bb8a0a7db599722bc8121d27feb2dc
Adrian Jablonski <adrian.jablonski19@gmail.com>
2020-01-28 20:12:51 -0500

    Connected date range picker to redux

--- Complete-React-Dev-Course/expensify/src/redux/selectors/expenses.js
+++ Complete-React-Dev-Course/expensify/src/redux/selectors/expenses.js
@@ -1,9 +1,12 @@
+import moment from "moment";
+
 // Get visible expenses
 const getVisibleExpenses = (expenses, {text, sortBy, startDate, endDate}) => {
     return expenses.filter((expense) => {
-        const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
-        const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;;
-        const textMatch = (text) ? (expense.description.toLowerCase()).includes(text.toLowerCase()) : true;
+        const createdAtMoment = moment(expense.createdAt);
+        const startDateMatch = startDate ? startDate.isSameOrBefore(createdAtMoment, 'day') : true;
+        const endDateMatch = endDate ? endDate.isSameOrAfter(createdAtMoment, 'day') : true;
+        const textMatch = (text) ? expense.description.toLowerCase().includes(text.toLowerCase()) : true;
 
         return startDateMatch && endDateMatch && textMatch;
     }).sort((a, b) => {
