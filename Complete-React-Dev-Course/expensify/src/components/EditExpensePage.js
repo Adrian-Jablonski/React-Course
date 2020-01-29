9580648b19c13789b5139842377f6ef0bd9e7084
Adrian Jablonski <adrian.jablonski19@gmail.com>
2020-01-28 19:14:48 -0500

    Setup edit expense page

--- Complete-React-Dev-Course/expensify/src/components/EditExpensePage.js
+++ Complete-React-Dev-Course/expensify/src/components/EditExpensePage.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import ExpenseForm from './ExpenseForm';
+import { editExpense } from '../redux/actions/expenses';
+import { removeExpense } from '../redux/actions/expenses';
 
-const EditExpensePage = (props) => {
-    console.log(props);
+const EditExpensePage = ({ dispatch, history, match, expense }) => {
     return (
-    <div>
-        Edit Expense component with id of {props.match.params.id}
-    </div>
-)
+        <div>
+            <ExpenseForm
+                expense={expense}
+                onSubmit={(expense) => {
+                    dispatch(editExpense(match.params.id, expense))
+                    history.push('/');
+                }}
+            />
+            <button
+                onClick={() => {
+                    dispatch(removeExpense({ id: match.params.id }));
+                    history.push('/');
+                }}
+            >Remove</button>
+        </div>
+    )
 }
-    
 
-export default EditExpensePage;
+const mapStateToProps = (state, props) => {
+    return {
+        expense: state.expenses.find((expense) => {
+            return expense.id === props.match.params.id;
+        })
+    }
+}
+
+export default connect(mapStateToProps)(EditExpensePage);
--- Complete-React-Dev-Course/expensify/src/components/ExpenseForm.js
+++ Complete-React-Dev-Course/expensify/src/components/ExpenseForm.js
@@ -5,13 +5,18 @@ import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css'
 
 class ExpenseForm extends Component {
-    state = {
-        description: '',
-        amount: '',
-        note: '',
-        createdAt: moment(),
-        calendarFocused: false,
-        error: null
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            description: props.expense ? props.expense.description : '',
+            amount: props.expense ? (props.expense.amount / 100).toString() : '',
+            note: props.expense ? props.expense.note : '',
+            createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
+            calendarFocused: false,
+            error: null
+        }
     }
 
     onFieldChange = (e, field) => {
