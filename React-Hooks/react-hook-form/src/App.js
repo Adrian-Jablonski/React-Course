af9b3c8b8661ec15e5d2b45c5ca24bdc6e7ce09c
Adrian Jablonski <adrian.jablonski19@gmail.com>
2020-02-19 21:01:50 -0500

    Added basic form validation

--- React-Hooks/react-hook-form/src/App.js
+++ React-Hooks/react-hook-form/src/App.js
@@ -1,37 +1,57 @@
-import React from "react";
-import useForm from "react-hook-form";
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const App = () => {
-  const onSubmit = data => {
+  const { register, handleSubmit, errors } = useForm();
+
+  const onSubmit = (data) => {
     alert(JSON.stringify(data));
   };
 
   return (
-    <form className="App" onSubmit={onSubmit}>
+    <form className='App' onSubmit={handleSubmit(onSubmit)}>
       <h1>Sign Up</h1>
       <label>First Name:</label>
-      <input name="firstName" />
+      <input name='firstName' ref={register({ required: true, minLength: 2 })} />
+      {errors.firstName && errors.firstName.type === 'required' &&
+        <p>This is required</p>
+      }
+      {errors.firstName && errors.firstName.type === 'minLength' &&
+        <p>This field required min length of 2</p>
+      }
 
       <label>Last Name:</label>
-      <input name="firstName" />
+      <input name='lastName' ref={register({ required: true })} />
+      {errors.lastName &&
+        <p>This is required</p>
+      }
 
       <label>Gender</label>
-      <select name="gender">
-        <option>Select...</option>
-        <option>Male</option>
-        <option>Female</option>
+      <select name='gender' ref={register({ required: true })}>
+        <option value=''>Select...</option>
+        <option value='male'>Male</option>
+        <option value='female'>Female</option>
       </select>
 
       <label>Username</label>
-      <input name="username" />
+      <input name='username' ref={register({ required: true })} />
+      {errors.username &&
+        <p>This is required</p>
+      }
 
       <label>Email</label>
-      <input name="email" />
+      <input name='email' ref={register({ required: true })} />
+      {errors.email &&
+        <p>This is required</p>
+      }
 
       <label>About you</label>
-      <textarea name="about you" />
+      <textarea name='aboutYou' ref={register({ required: true })} />
+      {errors.aboutYou &&
+        <p>This is required</p>
+      }
 
-      <input type="submit" />
+      <input type='submit' />
     </form>
   );
 }
