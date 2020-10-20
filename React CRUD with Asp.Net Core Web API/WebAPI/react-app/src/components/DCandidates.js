8d57c49c7e1313a59e3a4eeafa02db0682363d57
Adrian Jablonski <adrian.jablonski19@gmail.com>
2020-10-20 19:52:34 -0400

    Created react table from ASP API and styled table using material-ui

--- React CRUD with Asp.Net Core Web API/WebAPI/react-app/src/components/DCandidates.js
+++ React CRUD with Asp.Net Core Web API/WebAPI/react-app/src/components/DCandidates.js
@@ -1,16 +1,58 @@
 import React, { useState, useEffect } from 'react';
+import { Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, withStyles } from '@material-ui/core';
 import { connect } from "react-redux";
 import * as actions from "../actions/dCandidate";
+import DCandidateForm from './DCandidateForm';
 
-const DCandidate = ({ dCandidateList, fetchAllDCandidates }) => {
+const styles = theme => ({
+	root: {
+		// Class override in material UI
+		"& .MuiTableCell-head" : {
+			fontSize: "1.25rem"
+		}
+	},
+	paper: {
+		margin: theme.spacing(2),
+		padding: theme.spacing(2)
+	}
+})
+
+const DCandidate = ({ classes, dCandidateList, fetchAllDCandidates }) => {
 	useEffect(() => {
 		fetchAllDCandidates();
 	}, [])
-	console.log(dCandidateList);
 	return (
-		<div>
-			DCandidate
-		</div>
+		<Paper className={classes.paper} elevation={3}>
+			<Grid container>
+				<Grid item xs={6}>
+					<DCandidateForm />
+				</Grid>
+				<Grid item xs={6}>
+					<TableContainer>
+						<Table>
+							<TableHead className={classes.root}>
+								<TableRow>
+									<TableCell>Name</TableCell>
+									<TableCell>Mobile</TableCell>
+									<TableCell>Blood Group</TableCell>
+								</TableRow>
+							</TableHead>
+							<TableBody>
+								{dCandidateList.map((record, index) => {
+									return (
+										<TableRow key={index} hover>
+											<TableCell>{record.fullName}</TableCell>
+											<TableCell>{record.mobile}</TableCell>
+											<TableCell>{record.bloodGroup}</TableCell>
+										</TableRow>
+									)
+								})}
+							</TableBody>
+						</Table>
+					</TableContainer>
+				</Grid>
+			</Grid>
+		</Paper>
 	);
 }
 
@@ -24,4 +66,5 @@ const mapActionToProps = {
 	fetchAllDCandidates: actions.fetchAll
 }
 
-export default connect(mapStateToProps, mapActionToProps)(DCandidate);
+// Styles from withStyles are accessed through the classes props
+export default connect(mapStateToProps, mapActionToProps)(withStyles(styles)(DCandidate));
