e4824e8e5a44520e80c94deadf16f6d5c839e20f
Adrian Jablonski <adrian.jablonski19@gmail.com>
2020-10-20 20:16:19 -0400

    Added form fields

--- React CRUD with Asp.Net Core Web API/WebAPI/react-app/src/components/DCandidateForm.js
+++ React CRUD with Asp.Net Core Web API/WebAPI/react-app/src/components/DCandidateForm.js
@@ -1,10 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Grid, TextField } from '@material-ui/core';
 
+const initialFieldValues = {
+	fullName: '',
+	mobile: '',
+	email: '',
+	age: '',
+	bloodGroup: '',
+	address: ''
+}
+
+// https://dev.to/maxbvrn/react-props-auto-complete-in-vs-code-2ana
+/**
+ * DCandidateForm to add to database
+ * @augments {Component<Props, State>}
+ */
 const DCandidateForm = () => {
+	const [values, setValues] = useState(initialFieldValues);
+
+	const handleInputChange = e => {
+		const { name, value } = e.target;
+		setValues({
+			...values,
+			[name]: value
+		})
+	}
 	return (
-		<div>
-			DCandidateForm
-		</div>
+		<form autoComplete="off" noValidate>
+			<Grid container>
+				<Grid item xs={6}>
+					<TextField
+						name="fullName"
+						variant="outlined"
+						label="Full Name"
+						value={values.fullName}
+						onChange={handleInputChange}
+					/>
+					<TextField
+						name="email"
+						variant="outlined"
+						label="Email"
+						value={values.email}
+						onChange={handleInputChange}
+					/>
+					<div>bloodGroup</div>
+				</Grid>
+				<Grid item xs={6}>
+					<TextField
+						name="mobile"
+						variant="outlined"
+						label="Mobile"
+						value={values.mobile}
+						onChange={handleInputChange}
+					/>
+					<TextField
+						name="age"
+						variant="outlined"
+						label="Age"
+						value={values.age}
+						onChange={handleInputChange}
+					/>
+					<TextField
+						name="address"
+						variant="outlined"
+						label="Address"
+						value={values.address}
+						onChange={handleInputChange}
+					/>
+				</Grid>
+			</Grid>
+		</form>
 	);
 }
 
