6f1e847462121c42587438549f135603c0817365
Adrian Jablonski <adrian.jablonski19@gmail.com>
2020-10-24 09:21:12 -0400

    Added test cases for header components

--- unit-testing-with-jest-and-enzyme/my-app/src/component/header/index.js
+++ unit-testing-with-jest-and-enzyme/my-app/src/component/header/index.js
@@ -3,10 +3,11 @@ import Logo from '../../assets/graphics/Logo.png';
 
 const Header = () => {
 	return (
-		<header>
+		// data-test attribute could be used to tell other devs to not change the value because it is used in tests
+		<header data-test="header-component">
 			<div className="wrap">
 				<div className="logo">
-					<img src={Logo} alt="logo" />
+					<img className="logo-img" src={Logo} alt="logo" />
 				</div>
 			</div>
 		</header>
---
+++ unit-testing-with-jest-and-enzyme/my-app/src/tests/header.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Header from '../component/header';
+import { findByTestAttr } from '../../Utils';
+
+const setUp = (props={}) => {
+	const component = shallow(<Header {...props} />);
+	return component;
+}
+
+describe('Header Component', () => {
+	let component;
+	beforeEach(() => {
+		component = setUp();
+	});
+
+	it('Should render without errors', () => {
+		const wrapper = findByTestAttr(component, 'header-component'); 
+		expect(wrapper.length).toBe(1);
+	});
+
+	it('Should render a logo with class logo-img', () => {
+		const logo = component.find('.logo-img'); // Looks for class logo-img
+		expect(logo.length).toBe(1);
+	});
+})
