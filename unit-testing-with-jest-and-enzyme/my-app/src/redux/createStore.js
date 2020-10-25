c5875ad8577a6b157872b4d452e1969f9ba6deea
Adrian Jablonski <adrian.jablonski19@gmail.com>
2020-10-25 13:18:23 -0400

    Added redux devtools extension

--- unit-testing-with-jest-and-enzyme/my-app/src/redux/createStore.js
+++ unit-testing-with-jest-and-enzyme/my-app/src/redux/createStore.js
@@ -1,10 +1,14 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import ReduxThunk from 'redux-thunk';
 import RootReducer from './reducers';
 
 export const middlewares = [ReduxThunk];
 
-export const createStoreWithMiddleware = applyMiddleware
-	(...middlewares)(createStore);
+export const createStoreWithMiddleware = createStore(
+	RootReducer, 
+	compose(applyMiddleware
+	(...middlewares),
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	));
 
-export const store = createStoreWithMiddleware(RootReducer);
+export const store = createStoreWithMiddleware;
